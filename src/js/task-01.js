dd4da92d924e49e0bd35cfd90c8c0ef9b4a9ec1e
PPacuk <piotrek.pacuk@gmail.com>
2022-11-30 22:29:53 +0100

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,15 @@
-const cat = document.querySelectorall('#categories');
-const list = cat.querySelectorall('.item')
+const categoriesNumber = document.querySelectorAll(".item");
+const categoriesNumberLength = categoriesNumber.length;
 
-list.forEach(element => {
-    console.log(element.textContent)
+console.log("Number of categories:", categoriesNumberLength);
+
+const category = document.querySelectorAll("h2");
+
+category.forEach((element) => {
+	console.log("Category:", element.textContent);
+	const categoryElements = document.querySelector("li > ul");
+
+	// categoryElements.forEach((element) => {
+	console.log("Elements:", categoryElements.children.length);
+	// });
 });
-
