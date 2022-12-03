ea8e3796ad2976a37741361bc35308d1935c8e93
PPacuk <piotrek.pacuk@gmail.com>
2022-12-03 20:12:32 +0100

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,15 +7,18 @@ const ingredients = [
 	"Condiments",
 ];
 const ingred = document.querySelector("#ingredients");
-const list = document.createElement(`li`);
+
+ingredients.forEach((item) => {
+	const ingredientsWithLi = document.createElement(`li`);
+	ingredientsWithLi.classList.add("item");
+	ingredientsWithLi.textContent = item;
+	console.log(ingredientsWithLi);
+	document.body.after(ingredientsWithLi);
+});
+
+// Też działa ale nie wiedziałem gdzie użyć createElement
 // const ingredientsWithLi = ingredients
 // 	.map((ingredienty) => `<li class="item">${ingredienty}</li>`)
 // 	.join("");
 
 // ingred.innerHTML = ingredientsWithLi;
-
-for (let i = 0; i < ingredients.length; i++) {
-	const element = list;
-}
-
-console.log();
