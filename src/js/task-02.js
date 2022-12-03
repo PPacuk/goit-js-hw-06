951e9dbdb0982d13cce093850faee3bf9bf59960
PPacuk <piotrek.pacuk@gmail.com>
2022-12-03 15:37:15 +0100

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,16 +7,17 @@ const ingredients = [
 	"Condiments",
 ];
 const ingred = document.querySelector("#ingredients");
-const list = document.createElement("li");
+// const list = document.createElement("li");
+const ingredientsWithLi = ingredients
+	.map((ingredienty) => `<li class="item">${ingredienty}</li>`)
+	.join("");
 
-list.classList.add("item");
-list.innerHTML = ingredients;
+ingred.innerHTML = ingredientsWithLi;
+// list.classList.add("item");
+// list.innerHTML = ingredients;
 
-for (const item of ingredients) {
-	
-}
+// for (const item of ingredients) {
 
+// }
 
-
-
-console.log(list);
+console.log(ingredientsWithLi);
