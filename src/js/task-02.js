6430c7fe45e9f25e0eaa83303ebacc935dd93d5b
PPacuk <piotrek.pacuk@gmail.com>
2022-12-03 14:34:09 +0100

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,7 +7,16 @@ const ingredients = [
 	"Condiments",
 ];
 const ingred = document.querySelector("#ingredients");
+const list = document.createElement("li");
+
+list.classList.add("item");
+list.innerHTML = ingredients;
 
 for (const item of ingredients) {
-  
+	
 }
+
+
+
+
+console.log(list);
