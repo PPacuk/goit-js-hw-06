b94900b4a593098b1fb66d31c884ec1b7a55a989
PPacuk <piotrek.pacuk@gmail.com>
2022-12-05 23:02:58 +0100

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,12 +1,12 @@
 const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
-const anonymous = "anonymous";
+
 textInput.addEventListener("input", (event) => {
-    if (textOutput.textContent.length === 0) {
-        textOutput.textContent = event.currentTarget.anonymous;
-    }
-    
-    textOutput.textContent = event.currentTarget.value;
-    console.log(textOutput.textContent);
-    console.log(textOutput.textContent.length);
+   if (textOutput.textContent.length === 0) {
+		textOutput.textContent = "anonymous";
+   } else {
+		textOutput.textContent = event.currentTarget.value;
+   }
+    // console.log(textOutput.textContent);
+    // console.log(textOutput.textContent.length);
 });
