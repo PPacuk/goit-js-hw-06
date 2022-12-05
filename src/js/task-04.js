47f192293f936d19f821988f381a6f71706f2763
PPacuk <piotrek.pacuk@gmail.com>
2022-12-05 21:10:02 +0100

    done 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,12 +1,11 @@
-const counterValue = 0;
-const btn = document.querySelector("button");
+let counterValue = 0;
+let value = document.querySelector("#value");
 const btnDown = document.querySelector('button[data-action="decrement"]');
 const btnUp = document.querySelector('button[data-action="increment"]');
-const handleClick = () => {
-    console.log("add +1")
-}
-btnUp.addEventListener("click", () => {
-	console.log("click");
-});
 
-console.log(btn);
+btnUp.addEventListener("click", () => {
+	value.innerHTML = counterValue += 1;
+});
+btnDown.addEventListener("click", () => {
+	value.innerHTML = counterValue -= 1;
+});
