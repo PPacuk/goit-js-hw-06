9cfbf16ecff51033481af082990a3ee9dbfbbc6d
PPacuk <piotrek.pacuk@gmail.com>
2022-12-07 21:35:09 +0100

    done 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
+
+const handleSubmit = (event) => {
+	event.preventDefault();
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (email.value === "" || password.value === "") {
+		return window.alert("Please fill in all the fields!");
+    }
+    console.log(`${email.value}, ${password.value}`);
+event.currentTarget.reset();
+};
+
+form.addEventListener("submit", handleSubmit);
+
 
