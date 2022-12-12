dd66a132b6ff2bad20b1d59590ec8ddb9fe3b209
PPacuk <piotrek.pacuk@gmail.com>
2022-12-12 19:02:35 +0100

    09/06 fixed

--- js/task-09.js
+++ js/task-09.js
@@ -1,11 +1,15 @@
 function getRandomHexColor() {
-	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+const body = document.querySelector("body");
+const spanTextColor = document.querySelector(".color");
+body.style.backgroundColor = "rgb(250, 250, 250)";
+spanTextColor.textContent = body.style.backgroundColor;
+
 const btn = document.querySelector(".change-color");
 const handleColor = () => {
-	const body = document.querySelector("body");
-	const spanTextColor = document.querySelector(".color");
-
 	body.style.backgroundColor = getRandomHexColor();
 	spanTextColor.textContent = body.style.backgroundColor;
 };
